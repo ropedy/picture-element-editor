9db8f39f4aa5bd518b88b78f41b1b4e72ab3562c
ropedy <ropedy@gmail.com>
2020-07-21 23:36:49 +0300

    Added quick sizing grid
    - Added quick sizing grid for powers of two for canvas size when creating new project.
    - Fixed active styling not working for buttons.

--- src/components/NewProject.js
+++ src/components/NewProject.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 import { newProject } from '../reducers/canvasReducer';
 import { hideBox, setProjectWidth, setProjectHeight } from '../reducers/actionBoxReducer';
@@ -8,6 +11,9 @@ import '../styles/NewProject.scss';
 
 const NewProject = () => {
   const dispatch = useDispatch();
+  const [ showQuickSizing, setShowQuickSizing ] = useState(false);
+  const [ sizeIndicator, setSizeIndicator ] = useState({ width: 'Width', height: 'Height' });
+  const [ quickSizeCoords, setQuickSizeCoords ] = useState(null);
   const { width, height } = useSelector(({ actionBox }) => actionBox.newProject);
 
   const createNew = () => {
@@ -31,9 +37,33 @@ const NewProject = () => {
     dispatch(setProjectHeight(clampedHeight));
   };
 
+  const hideQuickSizing = evt => {
+    const mouseAreas = document.querySelectorAll('.expand-indicator, .quick-sizing-div');
+    const mouseAreasArray = [ ...mouseAreas ];
+
+    if (!mouseAreasArray.some(el => el.contains(evt.relatedTarget))) {
+      setShowQuickSizing(false);
+      setQuickSizeCoords(null);
+      setSizeIndicator({ width: 'Width', height: 'Height' });
+    }
+  };
+
+  const setSizeCoordinates = coords => {
+    setQuickSizeCoords(coords);
+    setSizeIndicator({
+      width: `W(${2 ** (coords.x + 2)})`,
+      height: `H(${2 ** (coords.y + 2)})`
+    });
+  };
+
+  const quickSizingClicked = coords => {
+    dispatch(setProjectWidth(2 ** (coords.x + 2)));
+    dispatch(setProjectHeight(2 ** (coords.y + 2)));
+  };
+
   return <div id='new-project'>
     <div>
-      <label>Width</label>
+      <label>{sizeIndicator.width}</label>
       <input
         type='number'
         value={width}
@@ -44,7 +74,7 @@ const NewProject = () => {
       />
     </div>
     <div>
-      <label>Height</label>
+      <label>{sizeIndicator.height}</label>
       <input
         type='number'
         value={height}
@@ -54,7 +84,36 @@ const NewProject = () => {
         max='32'
       />
     </div>
-    <button disabled={!sizeIsValid()} onClick={createNew}>Create new</button>
+    <div className='new-button-wrapper'>
+      <button disabled={!sizeIsValid()} onClick={createNew}>Create new</button>
+    </div>
+    <div onMouseEnter={() => setShowQuickSizing(true)} onMouseLeave={hideQuickSizing} className='expand-indicator'>
+      <FontAwesomeIcon className='icon' icon={faAngleDown} />
+    </div>
+    { showQuickSizing ? <div onMouseLeave={hideQuickSizing} className='quick-sizing-div'>
+      <table>
+        <tbody>
+          {[0,1,2,3].map((y, i) => {
+            return <tr key={i}>
+              {[0,1,2,3].map((x, j) => {
+                let highlight = false;
+
+                if (quickSizeCoords) {
+                  highlight = quickSizeCoords.x >= x && quickSizeCoords.y >= y;
+                }
+
+                return <td
+                  key={j}
+                  className={highlight ? 'highlight' : ''}
+                  onMouseEnter={() => setSizeCoordinates({ x, y })}
+                  onClick={() => quickSizingClicked({ x, y })}
+                />;
+              })}
+            </tr>;
+          })}
+        </tbody>
+      </table>
+    </div> : null}
   </div>;
 };
 
