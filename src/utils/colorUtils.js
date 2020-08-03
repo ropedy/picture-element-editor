62338bdf9b2a263a483d49cf97b75b8b9c20b24b
ropedy <ropedy@gmail.com>
2020-08-03 21:17:24 +0300

    Fixed isLight function

--- src/utils/colorUtils.js
+++ src/utils/colorUtils.js
@@ -6,6 +6,8 @@ export const isLight = color => {
   const red = parseInt(color.slice(1, 3), 16);
   const green = parseInt(color.slice(3, 5), 16);
   const blue = parseInt(color.slice(5, 7), 16);
+  const min = Math.min(red, green, blue);
+  const max = Math.max(red, green, blue);
 
-  return 382 > red + green + blue;
+  return 127 > ((min + max) / 2);
 };
