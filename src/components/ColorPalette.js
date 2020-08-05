1d2c4ce0514d1d4627958843269104d6a6c47436
ropedy <ropedy@gmail.com>
2020-08-05 20:42:48 +0300

    Removed unused imports

--- src/components/ColorPalette.js
+++ src/components/ColorPalette.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import PaletteColor from './PaletteColor';
